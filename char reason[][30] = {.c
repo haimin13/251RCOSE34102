fd91fc3307e3b56e5aa847f0b5c5e023ad268bb5
haimin13 <gkals862@naver.com>
2025-05-20 01:02:47 +0900

    쓸데없는 파일 삭제

--- char reason[][30] = {.c
+++
@@ -1,9 +0,0 @@
-char reason[][30] = {
-    "(Scheduler Start)",
-    "(Process Finished)",
-    "(I/O Requested)",
-    "(Time Slice Expired)",
-    "(Ready Queue Filled)",
-    "(Preempted)",
-    "(All Process Finished)"
-};
